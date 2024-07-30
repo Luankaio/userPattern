74ba04cc601930826326c86a265ef04bbdd4fb8e
luankaio <luankaiosilveira45@hotmail.com>
2024-07-30 11:50:23 -0300

    security

--- src/main/java/com/example/userpattern/models/User.java
+++ src/main/java/com/example/userpattern/models/User.java
@@ -1,7 +1,20 @@
 package com.example.userpattern.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue
     private UUID id;
+    private String name;
+
 }
